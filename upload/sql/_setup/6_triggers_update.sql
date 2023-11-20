5ea3746a7c5d85421df6bd0b0901d906c1b2c949
Joe <joceline.putra@gmail.com>
2023-11-20 15:56:59 +0700

    Update trigger

---
+++ upload/sql/_setup/6_triggers_update.sql
@@ -0,0 +1,71 @@
+DELIMITER $$
+DROP TRIGGER IF EXISTS `tr_order_paid_after_insert`$$ 
+CREATE TRIGGER `tr_order_paid_after_insert` AFTER INSERT ON `orders_paids`
+FOR EACH ROW 
+BEGIN
+   IF NEW.paid_order_id IS NOT NULL THEN
+        UPDATE orders AS o
+        LEFT OUTER JOIN (
+            SELECT paid_order_id, IFNULL(SUM(paid_total),0) AS paid_total FROM orders_paids
+            WHERE paid_order_id=NEW.paid_order_id
+        ) AS p ON p.paid_order_id=o.order_id
+        SET o.order_total_paid = p.paid_total,
+        o.order_paid = (
+        CASE 
+            WHEN o.order_total <= p.paid_total THEN '1'
+            WHEN o.order_total >= p.paid_total THEN '0'
+            ELSE '0'
+            END
+        )
+        WHERE o.order_id = NEW.paid_order_id;
+   END IF;   
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS `tr_order_paid_after_update`$$ 
+CREATE TRIGGER `tr_order_paid_after_update` AFTER UPDATE ON `orders_paids`
+FOR EACH ROW 
+BEGIN
+   IF NEW.paid_order_id IS NOT NULL THEN
+        UPDATE orders AS o
+        LEFT OUTER JOIN (
+            SELECT paid_order_id, IFNULL(SUM(paid_total),0) AS paid_total FROM orders_paids
+            WHERE paid_order_id=NEW.paid_order_id
+        ) AS p ON p.paid_order_id=o.order_id
+        SET o.order_total_paid = p.paid_total,
+        o.order_paid = (
+        CASE 
+            WHEN o.order_total <= p.paid_total THEN '1'
+            WHEN o.order_total >= p.paid_total THEN '0'
+            ELSE '0'
+            END
+        )
+        WHERE o.order_id = NEW.paid_order_id;
+   END IF;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS `tr_order_paid_after_delete`$$ 
+CREATE TRIGGER `tr_order_paid_after_delete` AFTER DELETE ON `orders_paids`
+FOR EACH ROW 
+BEGIN
+   IF OLD.paid_order_id IS NOT NULL THEN
+        UPDATE orders AS o
+        LEFT OUTER JOIN (
+            SELECT paid_order_id, IFNULL(SUM(paid_total),0) AS paid_total FROM orders_paids
+            WHERE paid_order_id=OLD.paid_order_id
+        ) AS p ON p.paid_order_id=o.order_id
+        SET o.order_total_paid = p.paid_total,
+        o.order_paid = (
+        CASE 
+            WHEN o.order_total <= p.paid_total THEN '1'
+            WHEN o.order_total >= p.paid_total THEN '0'
+            ELSE '0'
+            END
+        )
+        WHERE o.order_id = OLD.paid_order_id;
+   END IF;   
+END $$
+DELIMITER ;
