a4778ab3b79858a4e57ed3a234242296f96ade3c
Yoceline Witaya <joceline.putra@gmail.com>
2024-10-15 22:19:38 +0700

    Cece, Front Model, Front Contr Belum Upload

--- upload/sql/_setup/6_triggers_update.sql
+++ upload/sql/_setup/6_triggers_update.sql
@@ -9,15 +9,13 @@ BEGIN
     
         UPDATE orders AS o
         LEFT OUTER JOIN (
-            SELECT paid_order_id, IFNULL(SUM(paid_total),0) AS paid_total FROM orders_paids
-            WHERE paid_order_id=NEW.paid_order_id
+            SELECT paid_order_id, IFNULL(SUM(paid_total),0) AS paid_total FROM orders_paids WHERE paid_order_id=NEW.paid_order_id
         ) AS p ON p.paid_order_id=o.order_id
         SET o.order_total_paid = p.paid_total,
-        o.order_paid = (
-        CASE 
+        o.order_paid = (CASE 
             WHEN o.order_total <= p.paid_total THEN '1'
             WHEN o.order_total >= p.paid_total THEN '0'
-            ELSE '0'
+            ELSE '4'
             END
         ), o.order_order_paid_count = mCOUNT
         WHERE o.order_id = NEW.paid_order_id;
@@ -30,20 +28,21 @@ DROP TRIGGER IF EXISTS `tr_order_paid_after_update`$$
 CREATE TRIGGER `tr_order_paid_after_update` AFTER UPDATE ON `orders_paids`
 FOR EACH ROW 
 BEGIN
+   DECLARE mCOUNT INT(50) DEFAULT 0;
    IF NEW.paid_order_id IS NOT NULL THEN
+        SELECT COUNT(*) INTO mCOUNT FROM orders_paids WHERE paid_order_id=NEW.paid_order_id;
+
         UPDATE orders AS o
         LEFT OUTER JOIN (
-            SELECT paid_order_id, IFNULL(SUM(paid_total),0) AS paid_total FROM orders_paids
-            WHERE paid_order_id=NEW.paid_order_id
+            SELECT paid_order_id, IFNULL(SUM(paid_total),0) AS paid_total FROM orders_paids WHERE paid_order_id=NEW.paid_order_id
         ) AS p ON p.paid_order_id=o.order_id
         SET o.order_total_paid = p.paid_total,
-        o.order_paid = (
-        CASE 
+        o.order_paid = (CASE 
             WHEN o.order_total <= p.paid_total THEN '1'
             WHEN o.order_total >= p.paid_total THEN '0'
-            ELSE '0'
+            ELSE '4'
             END
-        )
+        ), o.order_order_paid_count = mCOUNT
         WHERE o.order_id = NEW.paid_order_id;
    END IF;
 END $$
